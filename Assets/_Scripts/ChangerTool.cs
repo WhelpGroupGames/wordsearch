85f2589cb9ca4c8e30480a586f1604fa94cf20cd
Sean Ryan <sryan@evercrack.com>
2018-01-19 01:03:57 -0500

    Add scene changer util as prefab

---
+++ Assets/_Scripts/ChangerTool.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangerTool : MonoBehaviour {
+
+    private int scene_count = 0;
+    private string[] scenes;
+    private List<Dropdown.OptionData> optionDataList = new List<Dropdown.OptionData>();
+    private Dropdown dd;
+
+
+    public void OnChangeClick()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(dd.captionText.text);
+    }
+
+    // Use this for initialization
+    void Start () {
+        scene_count = UnityEngine.SceneManagement.SceneManager.sceneCountInBuildSettings;
+        scenes = new string[scene_count];
+
+        // populate the dropdown options
+        for (int i = 0; i < scene_count; i++ )
+        {
+            scenes[i] = System.IO.Path.GetFileNameWithoutExtension(UnityEngine.SceneManagement.SceneUtility.GetScenePathByBuildIndex(i));
+            optionDataList.Add(new Dropdown.OptionData(scenes[i]));
+        }
+
+        dd = GetComponentInChildren<Dropdown>();
+        dd.ClearOptions();
+        dd.AddOptions(optionDataList);
+                 
+	}
+
+}
---
+++ Assets/_Scripts/GameController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Singleton Data / Controller class that exists to share data
+/// between scenes.
+/// </summary>
+public class GameController : MonoBehaviour {
+
+    public static GameController instance;
+
+    void Awake() {
+        // if nothing exists, create it.  If it already exists
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if(instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+	// Use this for initialization
+	void Start () {
+        //This shit doesn't work right
+        //Debug.Log("Total Scenes: " + SceneManager.sceneCountInBuildSettings);
+        //for (int i = 0; i < SceneManager.sceneCountInBuildSettings; i++ )
+        //{
+        //    Debug.Log("Scene : " + i + ", " + SceneManager.GetSceneByBuildIndex(i).name);
+        //}
+    }
+
+    // Update is called once per frame
+    void Update() { }
+}
